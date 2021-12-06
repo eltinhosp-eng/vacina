2fca59d74ae3c4b7fa573bf4b540b20b75af7218
Elton Santos <elton.c.s1979@gmail.com>
2021-12-06 20:19:25 -0300

    Criação de tabelas Banco de Dados

--- vacina/notificacao_vacina/models.py
+++ vacina/notificacao_vacina/models.py
@@ -7,3 +7,20 @@ class Vacina(models.Model):
     codigo = models.IntegerField(default=0)
     dose = models.IntegerField(default=0)
 
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    celular = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+
+
+class Notificacao(models.Model):
+    RESPOSTAS = (
+        ('S', 'Sim'),
+        ('N', 'Não')
+    )
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    notificacao_celular = models.CharField(max_length=1, choices=RESPOSTAS)
+    notificacao_email = models.CharField(max_length=1, choices=RESPOSTAS)
+
