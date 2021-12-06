b4956ace54477eced6efe30794ba982b6af2bc03
Elton Santos <elton.c.s1979@gmail.com>
2021-12-06 19:04:25 -0300

    Configuração do Django

--- vacina/notificacao_vacina/admin.py
+++ vacina/notificacao_vacina/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vacina
+
+admin.site.register(Vacina)
+
--- vacina/notificacao_vacina/views.py
+++ vacina/notificacao_vacina/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
-
+import urllib.request
+from bs4 import BeautifulSoup
 
 def index(request):
+    html = urllib.request.urlopen("https://vacinacovid.saobernardo.sp.gov.br/VacinaSBCProd/servlet/inicial")
+    bs = BeautifulSoup(html, 'html.parser')
+    buscar_faixas(3, bs)
     return HttpResponse("Validação do Django em app.vacina")
+
+
+
+def buscar_faixas(dose_final, bs):
+   for dose in range(1, dose_final + 1):
+        id_select = f"vPUBLICOALVOVACINAID_D{dose}"
+        select = bs.find('select', id=id_select)
+        options = select.find_all('option')
+        print(f"Vacinação Dose{dose}")
+        for option in options:
+            if option['value'] != "":
+             print(f"ID: {option['value']} DESCRIÇÃO: {option.text}")
+        print("*******************")
+
